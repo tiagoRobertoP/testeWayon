235d3fd885ae7d131d23199ef7d02d3ecb9d8c97
Tiago Roberto <tiagowebwork@gmail.com>
2025-04-15 10:33:52 -0300

    init

---
+++ src/main/java/com/teste/wayonTiago/WayonTiagoApplication.java
@@ -0,0 +1,13 @@
+package com.teste.wayonTiago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayonTiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayonTiagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/wayonTiago/WayonTiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.wayonTiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayonTiagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
